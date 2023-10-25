4ac0cb8f452cf7ad67bfffb197fc148b1e91a4c9
Jon Phillips <Jphillips39@cnm.edu>
2023-10-25 16:58:49 -0600

    addes wemo started on hue

--- src/smart-room-controller.cpp
+++ src/smart-room-controller.cpp
@@ -13,9 +13,12 @@
 #include <colors.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_SSD1306.h>
+#include <hue.h>
 
 
-SYSTEM_MODE(SEMI_AUTOMATIC);
+
+
+SYSTEM_MODE(MANUAL);
 
 
 const int BUZZER = D13;
@@ -34,28 +37,39 @@ int bmeStatus;
 int encRead;
 int thermoStat;
 int thermoSet;
+int fanOn;
+int heatOn;
+int i;
+int currTime;
+int prevTime;
 char degree = 0xF8;
-
-
-
-
+bool heat;
+bool cool;
+bool lftIsclicked;
+bool rtIsclicked;
+bool encIsclicked;
+const int wemoHeat=0;
+const int wemoCool=3;
+Button leftButton (D3); 
+Button rightButton (D4);
+Button encButton (D15);
 
 
 void setup() {
+    
+
     Serial.begin(9600);
+    waitFor(Serial.isConnected,15000);
 
-    // Serial.begin(9600);
-    // waitFor(Serial.isConnected,15000);
+    WiFi.on();
+    WiFi.clearCredentials();
+    WiFi.setCredentials("IoTNetwork");
 
-    // WiFi.on();
-    // WiFi.clearCredentials();
-    // WiFi.setCredentials("IoTNetwork");
-
-    // WiFi.connect();
-    // while(WiFi.connecting()){
-    //     Serial.printf(".");
-    // }
-    // Serial.printf("\n\n");
+    WiFi.connect();
+    while(WiFi.connecting()){
+        Serial.printf(".");
+    }
+    Serial.printf("\n\n");
 
     pinMode(BUZZER,OUTPUT);
 
@@ -86,11 +100,28 @@ void loop() {
 
 
 
-
+currTime = millis();
 tempC = bme.readTemperature();
 tempF = map(tempC,0,100,32,212);
 encRead = enCoder.read();
 thermoSet = map(encRead,0,96,60,100);
+cool = false;
+heat = false;
+fanOn = thermoSet+3;
+heatOn = thermoSet-3;
+pixel.clear();
+noTone(BUZZER);
+
+
+
+
+if(tempF<heatOn){
+    heat = true;
+}
+
+if(tempF>fanOn){
+    cool = true;
+}
 
 if(encRead<0){
     enCoder.write(0);
@@ -101,8 +132,33 @@ if(encRead>96){
     encRead = 96;
 }
 
+if(heat){
+  
+    // wemoWrite(wemoHeat,HIGH);
+    pixel.setPixelColor(0,red);
+    pixel.show();
+    tone(BUZZER,2000,500);
+}
+    
+
+if(cool){
+
+    wemoWrite(wemoCool,HIGH);
+    pixel.setPixelColor(1,blue);
+    pixel.show();
+    tone(BUZZER,1000,500);
+}
+
+if (cool==false && heat==false){
+    wemoWrite(wemoCool,LOW);
+    wemoWrite(wemoHeat,LOW);
+    pixel.clear();
+    pixel.show();
+    noTone(BUZZER);
+}
+   
 display.setTextSize(2);
-display.printf("Room Temp\n");SYSTEM_MODE(SEMI_AUTOMATIC);
+display.printf("Room Temp\n");
 display.setTextSize(3);
 display.printf("%i%cF\n",tempF,degree);
 display.setTextSize(1);
@@ -111,31 +167,45 @@ display.display();
 display.clearDisplay();
 display.setCursor(0,0);
 
-Serial.printf("tempF = %i\n thermoSet = %i\n",tempF,thermoSet);
-
-while((tempF-3)<thermoSet){
-    pixel.setPixelColor(0,red);
-    pixel.show();
-    tone(BUZZER,1000,1000);    
+if(leftButton.isClicked()){
+    lftIsclicked = !lftIsclicked;     
 }
-    // else{
-    //     pixel.clear();
-    //     pixel.show();
-    //     noTone(BUZZER);
-    // }
 
-while((tempF+3)>thermoSet){
-    pixel.setPixelColor(1,blue);
-    tone(BUZZER,2000,1000);
+
+if(lftIsclicked){
+    for(i=1;i<7;i++){
+        setHue(4,true,225,100,0.33);
+    }
     
+
+    
+
 }
-    // else{
-    //     pixel.clear();
-    //     pixel.show();
-    //     noTone(BUZZER);
-    // }
+    else{
+        for(i=1;i<7;i++){
+            setHue(4,false);
+        }
+
+    }
 
 
+if(lftIsclicked);
+    if(rightButton.isClicked()){
+        rtIsclicked = !rtIsclicked;
+    }
 
+    if(rtIsclicked){
+        for(i=1;i<8;i++){
+            setHue(4,true, HueRainbow[random(8,0)],50,255);
+        }
+    }
+    else{
+        for(i=1;i<8;i++){
+            setHue(4,true,255,100,0.33);
+        }
+    }
+
+
+    
 
 }
